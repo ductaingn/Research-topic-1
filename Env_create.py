7f37df8614fb645b5ca68be8cbbd3cf81c4cd285
R4C00n <superc0605@gmail.com>
2023-06-12 21:38:23 +0700

    Update h_bkf, gamma_bfk, r_bkf

--- Env_create.py
+++ Env_create.py
@@ -1,4 +1,3 @@
-#create environment for the problem
 import numpy as np
 import random as rd
 
@@ -10,13 +9,24 @@ length_of_cell = 1
 number_of_area_per_row = 3
 length_of_area = length/number_of_area_per_row
 
+#Number of APs
+NUM_OF_AP=9
+#Number of Users
+NUM_OF_USER=10
+#Transmit Power P = 5dBm 
+P = 5
+#Noise Power sigma^2 = -169dBm/Hz
+SIGMA_SQRT = -169
+#Bandwidth W = 10MHz
+W = 10
+
 #the list contains the position of APs
 list_of_AP = []
 
 #initialize position of each AP.
 #each AP was located at the central of each area
 #the position of each AP is the constant
-for i in range(9): 
+for i in range(NUM_OF_AP): 
     list_of_AP.append((i % 3 * length_of_area + length_of_area / 2 , (i - i%3) / 3 * length_of_area + length_of_area / 2))
     print(list_of_AP[i])
 
@@ -31,20 +41,49 @@ def distance_to_nearest_AP(pos_of_user, list_of_AP):
 
 
 #initialize user's postion with random value
-#the position of users are constants
-list_of_users = []
-
 #after initializing any user's position, check the distance from that user to the nearest AP,
-#if the distance is satisfied, store it into the array list_of_U.
-i = 0
-while i < 10:
-    # sinh như này là ngẫu nhiên trên từng phần thôi, không phải ngẫu nhiên trên toàn cục không gian mẫu
-    area_index = i%3
-    # list_of_users.append((rd.uniform(area_index * length_of_area, (area_index + 1)*length_of_area), rd.uniform(area_index * length_of_area, (area_index + 1)*length_of_area)))
-    list_of_users.append((rd.uniform(0,length),rd.uniform(0,width)))
-    if(distance_to_nearest_AP(pos_of_user= list_of_users[i], list_of_AP= list_of_AP) >= 1):
-        print(list_of_users[i])
-        i = i+1
-    else:
-        list_of_users.remove(list_of_users[i])
+#if the distance is satisfied, store it into the array list_of_users.
+def initialize_users_pos():
+    list_of_users = []
+    i = 0
+    while i < 10:
+        list_of_users.append((rd.uniform(0,length),rd.uniform(0,width)))
+        if(distance_to_nearest_AP(pos_of_user= list_of_users[i], list_of_AP= list_of_AP) >= 1):
+            i = i+1
+        else:
+            list_of_users.remove(list_of_users[i])
+    return list_of_users
+list_of_users=initialize_users_pos()
 
+
+#channel coefficient h = a + b*i
+#in which a and b is random value from a Normal distribution
+def generate_h(mu,sigma):
+    re=np.random.normal(mu,sigma,1)[0]
+    im=np.random.normal(mu,sigma,1)[0]
+    h=complex(re,im)
+    return h
+
+#return a matrix of channel coefficient h between user k and AP b
+def initialize_users_h():
+    list_of_users_h=np.matrix(np.zeros([NUM_OF_AP,NUM_OF_USER]),dtype=complex)
+    for b in range(NUM_OF_AP):
+        for k in range (NUM_OF_USER):
+            list_of_users_h[b,k]=generate_h(0,0.1)
+    return list_of_users_h
+
+# gamma_bkf (t) is the Signal to Interference-plus-Noise Ratio (SINR)
+# at user k for the transmit signal from AP b, application f
+def gamma(AP_index,user_index,application_index):
+    h=initialize_users_h()
+    power=pow(abs(h[AP_index,user_index]),2)*P
+    interference_plus_noise=0
+    for b in range(list_of_AP.__len__()):
+        if(b!=AP_index):
+            interference_plus_noise+=(pow(abs(h[b,user_index]),2)*P+W*SIGMA_SQRT)
+    return power/interference_plus_noise
+
+#achievable data rate r_bkf (t) for the link between
+# AP b, user k and for application f using bandwidth Wf at scheduling frame t
+def r(AP_index,user_index,application_index):
+    return W*np.log2(1+gamma(AP_index,user_index,application_index))
